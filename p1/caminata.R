687f949992fa2bbea83a2cc94c8a5bc3802ed07c
RParga <30968627+RParga@users.noreply.github.com>
2017-08-13 23:07:08 -0500

    Add files via upload

---
+++ p1/caminata.R
@@ -0,0 +1,18 @@
+caminata <- function(dim, dur, dist) {
+    pos <- rep(0, dim)
+    origen <- rep(0, dim)
+    contador <- 0
+    for (t in 1:dur) {
+        cambiar <- sample(1:dim, 1)
+        cambio <- 1
+        if (runif(1) < 0.5) {
+            cambio <- -1
+        }
+        pos[cambiar] <- pos[cambiar] + cambio
+        d <- dist(pos)
+        if (pos[1] == origen[1] & pos[2] == origen[2]) {
+            contador <-  contador + 1
+        }
+    }
+    return(contador)
+}
