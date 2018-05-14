f7747d3d249949656fe3f69df6499bfe68060373
RParga <30968627+RParga@users.noreply.github.com>
2018-05-13 20:03:17 -0500

    Add files via upload

---
+++ p12/comparacion1.R
@@ -0,0 +1,44 @@
+resultados<-data.frame()
+
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+negro<-0.995
+gris<-0.92
+blanco<-0.002
+
+for (replica in 1:10){
+  for (t.pruebas in seq(300,900,300)){
+    
+  source('P12.R')
+  secuencial<-cbind(replica,"Original",t.pruebas,tiempo,acierto)
+  
+  source('P12_T12.R')
+  paralelo<-cbind(replica,"Paralelo",t.pruebas,tiempo,acierto)
+
+  resultados<-rbind(resultados,secuencial,paralelo)
+   }
+  print(replica)
+}
+
+stopImplicitCluster()
+
+save.image(file="Tarea_P12.RData")
+#load("~/GitHub/Simulacion/Simulacion/P12/Codigo/Tarea_P12.RData")
+colnames(resultados)<-c("Replica","Tipo","Pruebas","Tiempo","Acierto")
+resultados$Tiempo<-as.numeric(levels(resultados$Tiempo))[resultados$Tiempo]
+resultados$Tipo<-as.factor(resultados$Tipo)
+resultados$Pruebas<-as.factor(resultados$Pruebas)
+
+library(ggplot2)
+ggplot()+
+  geom_boxplot(data=resultados,aes(x=Pruebas,y=Tiempo,fill=Tipo))+
+  ylab("Tiempo(s)")+xlab("Número de pruebas")
+ggsave("Variacion_tiempo.png")
+
+
+resultados$Acierto<-as.numeric(levels(resultados$Acierto))[resultados$Acierto]
+
+ggplot()+
+geom_boxplot(data=resultados,aes(x=Pruebas,y=Acierto,fill=Tipo))+
+ylab("Porcentajes (%)")+xlab("Número de pruebas")
+ggsave("Variacion_acierto.png")
